197324df8fc24770988450a67819f8175911dcbe
Niedja Araujo <niaraujos.1007@gmail.com>
2025-07-05 19:24:45 -0300

    Add files via upload
    estruturas de if else, listas, tuplas  e dicionario. laços de repetição whilw e for

---
+++ estruturas.py
@@ -0,0 +1,48 @@
+# tomadas de decisão estruturas de repetição
+
+#if, else - sintaxe e identação
+
+"""idade = 33
+if idade > 18:
+    print('passou')
+else:
+    print('não passou')"""
+
+#loop while
+"""while idade > 18:
+   execução"""
+
+#loop for
+
+"""frutas =['maça', 'banana','goiaba']
+for fruta in frutas:
+    print(fruta)"""
+
+#listas []
+# declarar uma lista
+""'lista_frutas = []'""
+
+#adicionar valor a uma lista
+"""lista_frutas.append('goiaba')
+print(lista_frutas)"""
+
+#adcionar valor do usuario
+"""fruta = input('digite uma fruta:' )
+lista_frutas.append(fruta)
+print(lista_frutas)"""
+
+#tuplas ()
+"""são listas que os valores nao podem ser alterados e usa-se parenteses ao inves de colchetes
+as tuplas sao raramente usadas, preferindo a lista.
+tupla = ('maça', 'banana')"""
+
+#dicionários {} 
+#declaração e estrutura
+"""dicionario = {'Chave': 'Valor'}
+
+dicionario = {}
+dicionario['Maça'] = 'É uma fruta que faz froinch'
+dicionario['Carro']= 'É uma automovel'
+print(dicionario)"""
+
+
